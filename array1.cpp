e4222cdb473d1a75cc87f6a0bd37570f64573aa1
RvnZe <88199470+RvnZe@users.noreply.github.com>
2022-11-15 08:51:04 +0700

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nilai[5]={2,4,9,15,22};
+
+    cout<<nilai[3]<<endl;	//nilai yang akan di tampilkan
+    cout<<nilai[1]<<endl;	//nilai yang akan di tampilkan
+
+    getch();
+}
